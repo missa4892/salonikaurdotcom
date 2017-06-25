323a7cb7e58cee17b4410817640e0ac56d489c86
bosskaur456 <missa4892@gmail.com>
2017-06-25 11:22:52 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static('frontend'));
+
+app.listen(port, () => {
+  console.log(`Started up at port ${port}`);
+});
+
+app.get('/', (req, res) => {
+  //res.send('<h1>Hello Express!</h1>');
+  res.sendFile(path.resolve('frontend/index.html'));
+});
