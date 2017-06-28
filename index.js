d930b5a008228753344dffbd2c990f80cb6de7a9
bosskaur456 <missa4892@gmail.com>
2017-06-28 23:44:26 +0800

    trying out differnt file paths

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ app.listen(port, () => {
 
 app.get('/', (req, res) => {
   //res.send('<h1>Hello Express!</h1>');
-  res.sendFile(path.resolve('frontend/index.html'));
+  res.sendFile(path.resolve('index.html'));
 });
