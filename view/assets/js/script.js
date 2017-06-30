cdcc955ed3493140f6d90946d85a9f8a75665bfb
bosskaur456 <missa4892@gmail.com>
2017-07-01 00:59:32 +0800

    rmove google map call

--- view/assets/js/script.js
+++ view/assets/js/script.js
@@ -180,6 +180,6 @@ var map=new google.maps.Map(document.getElementById("googleMap")
   ,mapProp);
 }
 
-google.maps.event.addDomListener(window, 'load', initialize);
+//google.maps.event.addDomListener(window, 'load', initialize);
 
 
